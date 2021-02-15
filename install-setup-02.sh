349fcb31e099fc4d2c726cd662bf4dddf617b2a1
anasir514 <anasir514@gmail.com>
2021-02-15 17:47:12 +0000

    Create install-setup-02.sh

---
+++ install-setup-02.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Ubuntu (GNOME) 18.04 setup script.
+
+dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebi
+
+# Initial Software
+
+sudo apt update
+
+sudo apt install net-tools htop lame git mc flatpak \
+gedit-plugin-text-size  nano \
+ubuntu-restricted-extras mpv vlc gnome-tweaks \
+gnome-tweak-tool qt5-style-plugins  synaptic -yy
+
+# Remove undesirable packages:
+
+sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-preferences -yy
+
+# Remove snaps and get packages from apt:
+
+sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
+sudo apt install gnome-characters gnome-calculator gnome-system-monitor \
+gnome-software-plugin-flatpak -yy
+
+sudo apt remove account-plugin-aim account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-twitter account-plugin-yahoo aisleriot brltty colord deja-dup deja-dup-backend-gvfs duplicity empathy empathy-common evolution-data-server-online-accounts example-content gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice-avmedia-backend-gstreamer libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-style-galaxy libreoffice-style-human libreoffice-writer libsane libsane-common mcp-account-manager-uoa python3-uno rhythmbox rhythmbox-plugins rhythmbox-plugin-zeitgeist sane-utils shotwell shotwell-common telepathy-gabble telepathy-haze telepathy-idle telepathy-indicator telepathy-logger telepathy-mission-control-5 telepathy-salut thunderbird thunderbird-gnome-support totem totem-common totem-plugins unity-lens-music unity-scope-audacious unity-scope-chromiumbookmarks unity-scope-clementine unity-scope-colourlovers unity-scope-devhelp unity-scope-firefoxbookmarks unity-scope-gdrive unity-scope-gmusicbrowser unity-scope-gourmet unity-scope-manpages unity-scope-musique unity-scope-openclipart unity-scope-texdoc unity-scope-tomboy unity-scope-video-remote unity-scope-virtualbox unity-scope-yelp unity-scope-zotero
+
+
+gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
+#set icons to minimize on click
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
+## Remove junk
+sudo apt-get remove thunderbird rhythmbox ubuntu-web-launchers -y
+
+
+## Text Editor
+#sudo snap install --classic code
+
+## Disable Apport
+sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+
+# Gotta reboot now:
+sudo apt update && sudo apt upgrade -y
+
+echo $'\n'$"*** All done! Please reboot now. ***"
+
